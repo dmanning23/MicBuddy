5b3c4f2157740d2292d152700abe2cf33dfd7694
Daniel Manning <daniel.manning@demandmedia.com>
2013-09-06 17:33:46 -0500

    android mic work

--- Android/Resources/Resource.designer.cs
+++ Android/Resources/Resource.designer.cs
@@ -9,9 +9,9 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: global::Android.Runtime.ResourceDesignerAttribute("MicBuddy.Android.Resource", IsApplication=false)]
+[assembly: global::Android.Runtime.ResourceDesignerAttribute("MicBuddyLib.Resource", IsApplication=false)]
 
-namespace MicBuddy.Android
+namespace MicBuddyLib
 {
 	
 	
